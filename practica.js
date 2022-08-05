5203d05777dddc3b63fb945c3100cfe6909b38dd
dpajaro98 <drpajaro7@misena.edu.co>
2022-08-05 13:14:47 -0500

    Ejercicio Numbers

---
+++ practica.js
@@ -0,0 +1,27 @@
+// Crea un archivo JS que contenga las siguientes líneas
+
+// - Una variable que contenga
+// tu altura en centímetros (entero)
+let alturaCm=172
+// - Una variable que contenga tu 
+//altura en metros (número de coma flotante)
+let alturaM=1.72
+// - Una variable que contenga tu peso en
+// kilogramos (número de coma flotante)
+let pesoKg=59.8
+// - Una variable que contenga tu altura
+// en metros redondeada hacia arriba
+let alturaRoundUpM=Math.round(alturaM)
+// - Una variable que contenga tu peso en 
+//kilogramos redondeado hacia abajo
+let pesoRoundLowKg=Math.floor(pesoKg)
+// - Una variable que contenga si "el máximo valor que 
+//se puede obtener en Javascript + 1" es igual al 
+//máximo valor que se puede obtener en Javascript
+let maxval=Number.MAX_VALUE+1
+let maxvalleg=Number.MAX_VALUE
+
+if(maxval==maxvalleg){
+    console.log(true)
+
+}
